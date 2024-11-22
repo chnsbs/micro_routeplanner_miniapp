38d532c56c0823f02c3015ddf54006e2fd463a0e
Cihan Subasi <cihansubasi.tr@gmail.com>
2024-11-22 20:37:26 +0100

    Create script.js

---
+++ script.js
@@ -0,0 +1,13 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const qrCodeElement = document.getElementById("qrcode");
+    const telegramUrl = "https://t.me/YourBotHandle";
+
+    new QRCode(qrCodeElement, {
+        text: telegramUrl,
+        width: 128,
+        height: 128,
+        colorDark: "#333",
+        colorLight: "#ffffff",
+        correctLevel: QRCode.CorrectLevel.H
+    });
+});
